0d5d3c39f8a04bf3f725cf3cc8e9b56d1db844c3
Arcee69 <olawalearcee@gmail.com>
2024-04-04 10:58:09 +0100

    App Clean Up

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      screens: {
+        xs: '300px',
+        sm: '600px', 
+        md: '700px',
+        lg: '1024px',
+        xl: '1400px'
+      },
+
+      fontFamily: {
+        'inter': ['Inter'],
+      },
+      colors: {
+        MODAL_BACKGROUND: "rgba(0, 0, 0, 0.23)",
+
+        BLACK: {
+          _100: "#000"
+        },
+        
+        WHITE: {
+          _100: "#FFF"
+        },
+
+        RED: {
+          _100: "#AF202D"
+        }
+        
+      }
+    },
+  },
+  plugins: [],
+}
