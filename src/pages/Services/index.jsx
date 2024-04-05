f45cebec816b481d656df0f716d060320fc8d8f9
Arcee69 <olawalearcee@gmail.com>
2024-04-05 10:14:41 +0100

    Hot Fix

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -60,107 +60,107 @@ const Services = () => {
           }
         </div>
         <div className='flex items-center justify-center mt-[165px] flex-col'>
-        <p className='w-[736px] text-center font-medium font-inter text-[67px] leading-[82px]'>
-            Let’s Collaborate in building a better future
-        </p>
-        <div className='mt-[100px] mb-[980px]'>
-            <Formik
-            initialValues={{
-                firstName: "",
-                lastName: "",
-                companyName: "",
-                workEmail: "",
-                country: ""
-            }}
-            // validationSchema={formValidationSchema}
-                onSubmit={(values, action) => {
-                window.scrollTo(0, 0);
-                console.log(values, "market")
-                submitForm(values, action);
-            }}
-            >
-            {({
-                handleSubmit,
-                handleChange,
-                dirty,
-                isValid,
-                setFieldValue,
-                errors,
-                touched,
-                // setFieldTouched,
-                values,
-            }) => (
-                <Form onSubmit={handleSubmit} className="flex ">
-                    <div className="flex flex-col gap-[21px]">
-                        <p className='text-[#19373E] font-inter font-medium text-[24px] text-center'>Join our growing community</p>
-                        
-                        <div className='flex items-center gap-[35px]'>
-                            <div className='flex flex-col gap-2 w-[276px]'>
-                            <input 
-                                name="firstName"
-                                placeholder="First Name"
-                                type='text'
-                                onChange={handleChange}
-                                className='w-[276px] h-[58px] border border-[#8F8F8F] p-2.5'
-                            />
-                            </div>
-                            <div className='flex flex-col gap-2 w-[276px]'>
-                            <input 
-                                name="lastName"
-                                placeholder="Last Name"
-                                type='text'
-                                onChange={handleChange}
-                                className='w-[276px] h-[58px] border border-[#8F8F8F] p-2.5'
-                            />
-                            </div>
-
-                        </div>
-
-                        <div className='w-[587px] flex flex-col gap-2'>
-                            <input 
-                            name="companyName"
-                            placeholder="Company Name"
-                            type='text'
-                            onChange={handleChange}
-                            className='w-[587px] h-[58px] border border-[#8F8F8F] p-2.5'
-                            />
-                        </div>
-
-                        <div className='w-[587px] flex flex-col gap-2'>
-                            <input 
-                            name="workEmail"
-                            placeholder="Work Email"
-                            type='text'
-                            onChange={handleChange}
-                            className='w-[587px] h-[58px] border border-[#8F8F8F] p-2.5'
-                            />
-                        </div>
-
-                        <div className='w-[587px] flex flex-col gap-2'>
-                            <input 
-                            name="country"
-                            placeholder="Country"
-                            type='text'
-                            onChange={handleChange}
-                            className='w-[587px] h-[58px] border border-[#8F8F8F] p-2.5'
-                            />
-                        </div>
-
-                        <button
-                            className="w-[129px] font-inter flex items-center justify-center mt-[26px] h-[46px] bg-[#000] text-lg rounded text-center"
-                            type="submit"
-                            disabled={loading}
-                        >
-                            <p className='text-WHITE-_100 text-sm font-semibold'>{loading ? <CgSpinner className=" animate-spin text-xl " /> : 'Submit'}</p>
+            <p className='w-[736px] text-center font-medium font-inter text-[67px] leading-[82px]'>
+                Let’s Collaborate in building a better future
+            </p>
+            <div className='mt-[100px] mb-[980px]'>
+                <Formik
+                initialValues={{
+                    firstName: "",
+                    lastName: "",
+                    companyName: "",
+                    workEmail: "",
+                    country: ""
+                }}
+                // validationSchema={formValidationSchema}
+                    onSubmit={(values, action) => {
+                    window.scrollTo(0, 0);
+                    console.log(values, "market")
+                    submitForm(values, action);
+                }}
+                >
+                {({
+                    handleSubmit,
+                    handleChange,
+                    dirty,
+                    isValid,
+                    setFieldValue,
+                    errors,
+                    touched,
+                    // setFieldTouched,
+                    values,
+                }) => (
+                    <Form onSubmit={handleSubmit} className="flex ">
+                        <div className="flex flex-col gap-[21px]">
+                            <p className='text-[#19373E] font-inter font-medium text-[24px] text-center'>Join our growing community</p>
                             
-                        </button>
-                    </div>
+                            <div className='flex items-center gap-[35px]'>
+                                <div className='flex flex-col gap-2 w-[276px]'>
+                                <input 
+                                    name="firstName"
+                                    placeholder="First Name"
+                                    type='text'
+                                    onChange={handleChange}
+                                    className='w-[276px] h-[58px] border border-[#8F8F8F] p-2.5'
+                                />
+                                </div>
+                                <div className='flex flex-col gap-2 w-[276px]'>
+                                <input 
+                                    name="lastName"
+                                    placeholder="Last Name"
+                                    type='text'
+                                    onChange={handleChange}
+                                    className='w-[276px] h-[58px] border border-[#8F8F8F] p-2.5'
+                                />
+                                </div>
 
-                </Form>
-            )}
+                            </div>
 
-            </Formik>
-        </div>
+                            <div className='w-[587px] flex flex-col gap-2'>
+                                <input 
+                                name="companyName"
+                                placeholder="Company Name"
+                                type='text'
+                                onChange={handleChange}
+                                className='w-[587px] h-[58px] border border-[#8F8F8F] p-2.5'
+                                />
+                            </div>
+
+                            <div className='w-[587px] flex flex-col gap-2'>
+                                <input 
+                                name="workEmail"
+                                placeholder="Work Email"
+                                type='text'
+                                onChange={handleChange}
+                                className='w-[587px] h-[58px] border border-[#8F8F8F] p-2.5'
+                                />
+                            </div>
+
+                            <div className='w-[587px] flex flex-col gap-2'>
+                                <input 
+                                name="country"
+                                placeholder="Country"
+                                type='text'
+                                onChange={handleChange}
+                                className='w-[587px] h-[58px] border border-[#8F8F8F] p-2.5'
+                                />
+                            </div>
+
+                            <button
+                                className="w-[129px] font-inter flex items-center justify-center mt-[26px] h-[46px] bg-[#000] text-lg rounded text-center"
+                                type="submit"
+                                disabled={loading}
+                            >
+                                <p className='text-WHITE-_100 text-sm font-semibold'>{loading ? <CgSpinner className=" animate-spin text-xl " /> : 'Submit'}</p>
+                                
+                            </button>
+                        </div>
+
+                    </Form>
+                )}
+
+                </Formik>
+            </div>
         </div>
 
     </div>
